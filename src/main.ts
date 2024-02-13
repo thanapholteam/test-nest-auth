127d5250604e555751378f56fe860acfd55f3fa4
Chaiyapat Oam <chaiyapat.meey@mail.kmutt.ac.th>
2024-02-14 02:44:18 +0700

    chore: core credential

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
-  app.enableCors({ origin: ['http://localhost:5173'] });
+  app.enableCors({ origin: ['http://localhost:5173'], credentials: true });
   await app.listen(3000);
 }
 bootstrap();
